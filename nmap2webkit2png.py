0aed8a770c2eafb0d04dc915fc6ca6555804fd32
bryanzinser <bryan.a.zinser@gmail.com>
2012-01-28 04:29:08 -0500

    fixed some fat finger errors

--- nmap2webkit2png.py
+++ nmap2webkit2png.py
@@ -5,22 +5,25 @@ iplist = []
 def runwebkit2png(a):
     i = 1
     p = {}
+
     for item in a:
-	    j = 1
+	    
+        j = 1
         arg = ['python', 'webkit2png.py', '-D', 'img/']
         ip = 'http://' + item
         arg.append(ip)
+        
         if i < 6:
             p[i] = subprocess.Popen(arg)
             i += 1
         else:
             time.sleep(6)
-            while j < 6
+            while j < 6:
                 p[j].terminate()
                 j += 1
             i = 1
             p = {}
-
+            
 def getformatedlist():
     i = 1
     while i <= 9:
