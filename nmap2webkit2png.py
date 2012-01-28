062483b4e429751891b1770c3089c2bbb1ea1f26
bryanzinser <bryan.a.zinser@gmail.com>
2012-01-28 04:40:23 -0500

    fixed up some whitespace stuff

--- nmap2webkit2png.py
+++ nmap2webkit2png.py
@@ -3,41 +3,36 @@ import subprocess, time, os, sys
 iplist = []
 
 def runwebkit2png(a):
-    i = 1
-    p = {}
-
-    for item in a:
-	    
-        j = 1
-        arg = ['python', 'webkit2png.py', '-D', 'img/']
-        ip = 'http://' + item
-        arg.append(ip)
-        
-        if i < 6:
-            p[i] = subprocess.Popen(arg)
-            i += 1
-        else:
-            time.sleep(6)
-            while j < 6:
-                p[j].terminate()
-                j += 1
-            i = 1
-            p = {}
+	i = 1
+	p = {}
+	for item in a:
+		j = 1
+		arg = ['python', 'webkit2png.py', '-D', 'img/']
+		ip = 'http://' + item
+		arg.append(ip)
+		if i < 6:
+			p[i] = subprocess.Popen(arg)
+			i += 1
+		else:
+			time.sleep(6)
+			while j < 6:
+				p[j].terminate()
+				j += 1
+			i = 1
+			p = {}
             
 def getformatedlist():
-    i = 1
-    while i <= 9:
-        arg = "log/linode-scan-%d" % i
-        indata = open(arg)
-        for line in indata:
-            if "open" in line:            
-                line = line.split(' ')
-                iplist.append(line[1].rstrip())
-        i += 1
-        indata.close()
-    return iplist
-
-
+	i = 1
+	while i <= 9:
+		arg = "log/linode-scan-%d" % i
+		indata = open(arg)
+		for line in indata:
+			if "open" in line:
+				line = line.split(' ')
+				iplist.append(line[1].rstrip())
+		i += 1
+		indata.close()
+	return iplist
 
 formatedlist = getformatedlist()
 runwebkit2png(formatedlist)
